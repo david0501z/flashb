979cce5b79c29557ce63a360cbdf65ef56e0e39f
david0501z <zhangdw0501@outlook.com>
2025-11-18 17:43:20 +0800

    Update protocol.dart

--- lib/common/protocol.dart
+++ lib/common/protocol.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:win32_registry/win32_registry.dart';
 
 class Protocol {
@@ -14,20 +13,23 @@ class Protocol {
 
   void register(String scheme) {
     String protocolRegKey = 'Software\\Classes\\$scheme';
-    RegistryValue protocolRegValue = RegistryValue(
-      name: 'URL Protocol',
-      type: RegistryValueType.string,
-      data: '',
-    );
-    String protocolCmdRegKey = 'shell\\open\\command';
-    RegistryValue protocolCmdRegValue = RegistryValue(
-      name: '',
-      type: RegistryValueType.string,
-      data: '"${Platform.resolvedExecutable}" "%1"',
-    );
-    final regKey = Registry.currentUser.createKey(protocolRegKey);
-    regKey.createValue(protocolRegValue);
-    regKey.createKey(protocolCmdRegKey).createValue(protocolCmdRegValue);
+    String protocolCmdRegKey = '$protocolRegKey\\shell\\open\\command';
+    
+    // 创建主协议键并设置值
+    final protocolKey = Registry.currentUser.createKey(protocolRegKey);
+    protocolKey.createValue(RegistryStringValue(
+      protocolKey.path,
+      'URL Protocol',
+      '',
+    ));
+    
+    // 创建命令键并设置值
+    final commandKey = Registry.currentUser.createKey(protocolCmdRegKey);
+    commandKey.createValue(RegistryStringValue(
+      commandKey.path,
+      '',  // 默认值名称
+      '"${Platform.resolvedExecutable}" "%1"',
+    ));
   }
 }
 
